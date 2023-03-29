c793b1b08bd0e9ddd0806d3be184bb831dc3be48
craftznora <sathminj1@gmail.com>
2023-03-29 17:20:15 +0530

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/sathminjanuth/" className="home__social-icon" target="_blank">
+        <i class='bx bxl-instagram-alt'></i>
+        </a>
+
+        <a href="https://www.youtube.com/thecodebase/" className="home__social-icon" target="_blank">
+        <i class='bx bxl-youtube'></i>
+        </a>
+
+        <a href="https://discord.gg/ftjMwuTdsR" className="home__social-icon" target="_blank">
+        <i class='bx bxl-discord-alt'></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
---
+++ src/components/work/Data.jsx
@@ -0,0 +1,53 @@
+import Work1 from "../../assets/project1.png";
+import Work2 from "../../assets/project2.png";
+import Work3 from "../../assets/project3.png";
+import Work4 from "../../assets/project4.png";
+import Work5 from "../../assets/project5.png";
+
+export const projectsData = [
+    {
+      id: 1,
+      image: Work1,
+      title: "The Code Base Website ",
+      category: "web",
+    },
+    {
+      id: 2,
+      image: Work2,
+      title: "VS Code Theme",
+      category: "design",
+    },
+    {
+      id: 3,
+      image: Work3,
+      title: "Screen Recorder",
+      category: "app",
+    },
+    {
+      id: 4,
+      image: Work4,
+      title: "Location Tracker",
+      category: "app",
+    },
+    {
+      id: 5,
+      image: Work5,
+      title: "Google Search Bar",
+      category: "web",
+    },
+  ];
+
+  export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'web',
+    },
+    {
+        name: 'app',
+    },
+    {
+        name: 'design',
+    },
+  ];
